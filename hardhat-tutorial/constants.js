146087713d53596154e73b8497d4654beb5bc220
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2022-05-13 15:09:21 -0400

    test

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,8 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x7D8053980D8A07A6400cAb56984D9B3A37379E3d";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
+// Addresses: 
+// FakeNFTMarketplace deployed to:  0xEaF3Dce582B883E05f5Ae52BfaC7151BDe1CCd2d
+// CryptoDevsDAO deployed to:  0x03dA1f2cC6e9aDfdb20d7c9977aB19bE72198Cae
